586a4669292b7a56b421b931dedf2ddd8e730ebd
tracy loisel <tracy.loisel@gmail.com>
2024-04-04 17:52:46 +0200

    fix railtie

--- lib/railtie.rb
+++ lib/railtie.rb
@@ -6,6 +6,10 @@ class Railtie < Rails::Railtie
 
   initializer 'create_or_update.include_in_application_record' do
     ::ActiveSupport.on_load(:active_record) do
+      # Ensure ApplicationRecord is loaded. This is necessary because in
+      # development mode, classes are not preloaded and are loaded on demand.
+      require_dependency 'application_record' unless defined?(::ApplicationRecord)
+
       ::ApplicationRecord.include(CreateOrUpdate)
     end
   end
